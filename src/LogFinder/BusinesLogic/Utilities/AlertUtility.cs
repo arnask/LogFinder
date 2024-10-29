8a8a5ee42479178b0a4335d16fe12dc38d275ce5
Arnas K <arnaskar@gmail.com>
2024-10-29 23:12:57 +0200

    Implement presentation and alerts sending

---
+++ src/LogFinder/BusinesLogic/Utilities/AlertUtility.cs
@@ -0,0 +1,27 @@
+﻿using LogFinder.BusinesLogic.Models;
+
+namespace LogFinder.BusinesLogic.Utilities;
+
+/// <summary>
+/// Utility which is responsible for triggering alerts.
+/// </summary>
+public static class AlertUtility
+{
+    /// <summary>
+    /// Triggers alerts by severity level.
+    /// </summary>
+    public static void TriggerAlert(List<RowDictionary> rows)
+    {
+        foreach (RowDictionary row in rows)
+        {
+            if (row.TryGetValue("severity", out var severity))
+            {
+                int severityLevel = int.Parse(severity);
+
+                Console.WriteLine(severityLevel is >= 1 and <= 10
+                    ? $"ALERT: [Severity {severityLevel}] found."
+                    : $"ALERT: unknown severity level: [Severity {severityLevel}].");
+            }
+        }
+    }
+}
--- src/LogFinder/Presentation/App.cs
+++ src/LogFinder/Presentation/App.cs
@@ -1,24 +1,82 @@
-﻿using LogFinder.BusinesLogic.Services;
+﻿using LogFinder.BusinesLogic.Models;
+using LogFinder.BusinesLogic.Services;
+using LogFinder.BusinesLogic.Utilities;
+using System.Text.Json;
 
 namespace LogFinder.Presentation;
 
 /// <summary>
 /// Main application execution point.
 /// </summary>
-public class App(IDataParserService dataParserService, IQueryExecutorService queryExecutorService)
+public class App(IQueryExecutorService queryExecutorService, IDataParserService dataParserService)
 {
+    private const string ChangeDirectoryArgument = "cd";
+    private const string AlertArgument = "alert";
+
+    private readonly JsonSerializerOptions _jsonOptions = new() { WriteIndented = true };
+
+    private List<RowDictionary> _data = [];
+
     /// <summary>
     /// Runs the application and enters the main execution loop.
     /// </summary>
     public void Run()
     {
-        while (true) {
-        var data = dataParserService.GetParsedData();
-        string q = "signatureId='*4608*'";
+        _data = dataParserService.GetParsedData();
 
-        queryExecutorService.ExecuteQuery(data, q);
-            Console.ReadLine();
+        while (true)
+        {
+            ShowConsoleHeader();
+            var userInput = Console.ReadLine();
+            try
+            {
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    continue;
+                }
+                else if (userInput.StartsWith(ChangeDirectoryArgument))
+                {
+                    string path = userInput[ChangeDirectoryArgument.Length..].TrimStart();
+                    _data = dataParserService.GetParsedData(path);
+                    Console.WriteLine($"Loaded data from directory: {path}.");
+                }
+                else
+                {
+                    bool shouldTriggerAlert = userInput.EndsWith(AlertArgument, StringComparison.OrdinalIgnoreCase);
+                    userInput = userInput.Replace(AlertArgument, "", StringComparison.OrdinalIgnoreCase);
+
+                    var queryResult = queryExecutorService.ExecuteQuery(_data, userInput);
+                    var jsonResult = JsonSerializer.Serialize(queryResult, _jsonOptions);
+                    Console.WriteLine(jsonResult);
+
+                    if (shouldTriggerAlert)
+                    {
+                        AlertUtility.TriggerAlert(queryResult.Result);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred: {ex.Message}.");
+            }
         }
     }
 
+    private static void ShowConsoleHeader()
+    {
+        Console.WriteLine("*********************************************************************************************************************");
+        Console.WriteLine("                                           Query Log Retrieval Instructions                                          ");
+        Console.WriteLine();
+        Console.WriteLine("Applicable query formats:");
+        Console.WriteLine("1. [column_name = 'search_string']");
+        Console.WriteLine("2. [SELECT FROM column_name WHERE text = 'search_string']");
+        Console.WriteLine(" - Example: [signatureId='Microsoft-Windows-Security-Auditing:4608'] or [SELECT FROM WHERE signatureId = 'Microsoft-Windows-Security-Auditing:4608']");
+        Console.WriteLine();
+        Console.WriteLine("To add an alert to severity levels, append 'alert' to your query:");
+        Console.WriteLine(" - Example: [column_name = 'search_string' alert] or [SELECT * FROM column_name WHERE text = 'search_string' alert]");
+        Console.WriteLine();
+        Console.WriteLine("To change the files directory, use the following command:");
+        Console.WriteLine(" - [cd path/to/csv/files]");
+        Console.WriteLine("*********************************************************************************************************************");
+    }
 }
